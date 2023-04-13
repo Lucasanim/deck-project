623675a1ad486ca528bf0d3810f42b52608bde05
lucas <lucasanim12@gmail.com>
2023-04-13 18:43:41 -0300

    Create Users, Discussions and Auth services

---
+++ discussions/src/test/java/com/deck/discussions/DiscussionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deck.discussions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscussionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
