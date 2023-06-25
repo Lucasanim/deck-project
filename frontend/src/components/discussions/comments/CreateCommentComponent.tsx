62f2bf422a1208209f7efe7352e9334ad6dc987f
lucas <lucasanim12@gmail.com>
2023-06-24 22:17:48 -0300

    Feat: Implement comment creation from UI WIP

--- frontend/src/components/discussions/comments/CreateCommentComponent.tsx
+++ frontend/src/components/discussions/comments/CreateCommentComponent.tsx
@@ -5,8 +5,39 @@ import Avatar from "@mui/material/Avatar";
 import { red } from "@mui/material/colors";
 import SendIcon from "@mui/icons-material/Send";
 import { Button, CardContent } from "@mui/material";
+import { createComment } from "../../../services/discussions/comments/CommentsService";
+import Comment from "../../../models/discussions/Comment";
 
-const CreateCommentComponent: React.FC = () => {
+interface Props {
+  discussionId: string | number;
+}
+
+const CreateCommentComponent: React.FC<Props> = (props: Props) => {
+  const MAX_TEXT_SIZE = 500;
+  const [body, setBody] = React.useState("");
+
+  const handleSend = async (event: React.ChangeEvent) => {
+    event.preventDefault();
+    const comment = {
+      creatorId: 0,
+      body: body,
+      createdAt: new Date(),
+    };
+    try {
+      await createComment(comment as Comment);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const handleBodyChange = (text: string) => {
+    let textBody = text;
+    if (text.length > MAX_TEXT_SIZE) {
+      textBody = text.slice(0, MAX_TEXT_SIZE);
+    }
+    setBody(textBody);
+  };
+
   return (
     <Card>
       <CardContent>
@@ -24,11 +55,17 @@ const CreateCommentComponent: React.FC = () => {
               label="Comment"
               multiline
               maxRows={4}
+              value={body}
+              onChange={(event) => handleBodyChange(event.target.value)}
             />
           </div>
         </div>
         <div className="flex justify-end mr-5">
-          <Button variant="contained" endIcon={<SendIcon />}>
+          <Button
+            variant="contained"
+            endIcon={<SendIcon />}
+            onClick={handleSend}
+          >
             Send
           </Button>
         </div>
--- frontend/src/services/discussions/comments/CommentsService.ts
+++ frontend/src/services/discussions/comments/CommentsService.ts
@@ -1,3 +1,4 @@
+import Comment from "../../../models/discussions/Comment";
 import CommentDetail from "../../../models/discussions/CommentDetail";
 import { GenericClient } from "../../genericClient/genericClient";
 
@@ -5,4 +6,8 @@ const instance = new GenericClient("/discussions/comment");
 
 export const fetchCommentsByDiscussionId = (discussionId: string | number) => {
     return instance.get<CommentDetail[]>(`/get-from-discussion/${discussionId}`)
+}
+
+export const createComment = (comment: Comment) => {
+    return instance.post("/", comment)
 }
